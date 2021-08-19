9cdd7af1b3b9f6e8261f5e508d1e66e623b8ceb2
Tyler Carolan <tylerpatrickc@gmail.com>
2021-08-19 11:03:37 -0500

    Created lottery contract

--- deploy.js
+++ deploy.js
@@ -2,7 +2,6 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile')
 
-
 const provider = new HDWalletProvider(
   'huge admit diamond winner noise pave grace novel recall walk choose learn',
   'https://rinkeby.infura.io/v3/e2790fb402004700bb40f44fc064a80b'
@@ -15,7 +14,7 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0], gasPrice: '5000000000' })
   
   console.log('Contract deployed to', result.options.address);
